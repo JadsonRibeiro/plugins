f3570ee6200c0c67e207bfc8c845f23a9d1fbe7b
Jadson Ribeiro <jds_tj@hotmail.com>
2015-07-13 12:11:10 -0300

    email sender: Cleaned Code

--- email_sender_to_shoppers/email_sender_to_shoppers.php
+++ email_sender_to_shoppers/email_sender_to_shoppers.php
@@ -18,7 +18,7 @@ add_action('send_email_event', 'print_order_on_period');
 */
 function email_sender_to_shoppers_activate() {
     if(!wp_next_scheduled('send_email_event')){
-        $time = rand(240, 360);
+        $time = rand(10, 20);
         update_option('email_sender_time_activation', $time_activation);
         wp_schedule_event(time()+$time, 'daily', 'send_email_event');
     } 
@@ -41,14 +41,14 @@ add_filter( 'wp_mail_content_type', 'set_content_type' );
 
 // SET EMAIL FROM ADDRESS
 function change_mail_from() {
-    return "cursos@email.com.br";
+    return "email_tal@email.com.br";
 }
  
 add_filter ("wp_mail_from", "change_mail_from");
  
 // SET EMAIL FROM NAME
 function change_from_name() {
-    return "Prof. Teacher";
+    return "Prof. Fulano de Tal";
 }
  
 add_filter ("wp_mail_from_name", "change_from_name");
@@ -96,7 +96,7 @@ function print_all_comerce_orders($date) {
         $template_header = "";
         $template_footer = "";
 
-        $url_plugin = plugins_url( '', __FILE__ );
+        $url_plugin = plugins_url( '', __FILE__ ).'/includes';
 
         $template_header = file_get_contents($url_plugin.'/template_email_avaliation_header.html');
         $template_header = str_replace('{plugin_url}', $url_plugin, $template_header);
@@ -122,64 +122,110 @@ function print_all_comerce_orders($date) {
         $template_footer = file_get_contents($url_plugin.'/template_email_avaliation_footer.html');
         
         $template = $template_header.''.$template_products.''.$template_footer;
-        echo $template;
+        //echo $template;
 
-        //email para log
-        $to = array("email@email.com", $user_email);
-        $subject = "Agradecimentos";
+        //email para teste
+        $to = array("email@emailteste.com");
+        $subject = "[TEST] O que achou do nosso produto?";
         $data = getdate();
 
-        //log
+        //LOG
         $plugin_path = plugin_dir_path( __FILE__ );
         $log_file = fopen($plugin_path.'log.txt', 'a+');
         chmod ($plugin_path.'log.txt', 0755);
 
-        if(wp_mail($to, $subject, $template, $header)) {
-            update_option('email_sender_activeted', 'email sent sucessfully on '.$data['hours'].':'.$data['minutes'].':'.$data['seconds']);
-            fwrite($log_file, 'o.id: '.$order_id.' email sent sucessfully to '.$user_email.'on '.$data['mday'].'/'.$data['mon'].'/'.$data['year'].' '. $data['hours'].':'.$data['minutes'].':'.$data['seconds']."\n");            
+        if(wp_mail($to, $subject, $template)) {
+            fwrite($log_file, 'o.id: '.$order_id.' email sent sucessfully to '.$user_email.'on '.$data['day'].'/'.$data['month'].'/'.$data['year'].' '. $data['hours'].':'.$data['minutes'].':'.$data['seconds']."\n");            
         } else {
-            update_option('email_sender_activeted', 'error on sent email on '.$data['hours'].':'.$data['minutes'].':'.$data['seconds']);
             fwrite($log_file, 'o.id: '.$order_id.' error on sent email to '.$user_email.'on '.$data['mday'].'/'.$data['mon'].'/'.$data['year'].' '. $data['hours'].':'.$data['minutes'].':'.$data['seconds']."\n");
         }
 
     }
 }
 
-function print_date($period){
-    
-    $today = getdate();
-    
-    $day = $today['mday'];
-    $month = $today['mon'];
-    $year = $today['year'];
-    
-    if($period == 'monthly') {
-        if($month == 1) {
-            $month = 12;
-            $year--;
-        } else {
-            $month--;
+function print_all_comerce_orders_emails($date) {
+
+    $args = array(
+        'post_type' => 'shop_order',
+        'post_status' => 'publish',
+        'posts_per_page' => -1,
+        'tax_query' => array(
+            array(
+                'taxonomy' => 'shop_order_status',
+                'field' => 'slug',
+                'terms' => array('completed')
+            )
+        ),
+        'date_query' => $date
+    );
+
+    $loop = new WP_Query($args);
+    $content = "";
+
+    wp_reset_postdata();
+
+    while($loop->have_posts()) {
+  
+        $loop->the_post();
+        $order_id = $loop->post->ID;
+        $order = new WC_Order($order_id);
+
+        $order_date = $order->order_date;
+        $user_id = $order->__get('user_id');
+        $user = get_user_by('id', $user_id);
+        $user_email = $user->user_email;
+        $user_name = $user->display_name;
+
+        $user_name = sanitize_name($user_name);
+
+        $template_thank_email = "";
+
+        $url_plugin = plugins_url( '', __FILE__ ).'/includes';
+
+        $template_thank_email = file_get_contents($url_plugin.'/template_email_thank.html');
+
+        $template_email_thank = str_replace('{user_name}', $user_name, $template_thank_email);
+
+        foreach($order->get_items() as $item) {
+            $item_name = $item['name'];
         }
-    } elseif($period == 'yearly') {
-        $year--;
+        $template_thank_email = str_replace('{product_name}', $item_name, $template_thank_email);
+
+        $to = 'email@emailteste.com';
+        $subject = '[TEST] Agredecimentos pela compra';
+        wp_mail($to, $subject, $template_thank_email);
     }
+}
+
+function print_date($period){
+
+
+    $before = '';
+
+    if($period == 'monthly') {
+        $before = "-1 month";
+    } elseif ($period == 'fortnightly') {
+        $before = "-15 days";
+    }
+    $today = getdate(strtotime($before));
     
     $date = array(
-            'year'  => $year,
-            'month' => $month,
-            'day'   => $day,
-        );
+        'day'  => $today['mday'],
+        'month'=> $today['mon'],
+        'year' => $today['year'],
+    );
 
     return $date; 
 }
 
 function print_order_on_period() {
-    $period = print_date('monthly');
-    
-    return print_all_comerce_orders($period);
-}
+    $period_email_coment = print_date('monthly');
+    $period_email_thank = print_date('fortnightly');
 
-add_shortcode('print_order_on_period','print_order_on_period');
+    print_all_comerce_orders_emails($period_email_thank);
+    
+    return print_all_comerce_orders($period_email_coment);
+}
 
 function sanitize_name($user_name) {
    
